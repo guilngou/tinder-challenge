d3d1e79a0e2c4262e729698710d06defebd05973
Alexandre <alexandreguilngar@gmail.com>
2018-11-20 22:16:41 +0100

    implemented meteor methods

--- meteor/imports/api/items.js
+++ meteor/imports/api/items.js
@@ -1,5 +1,41 @@
 import { Mongo } from "meteor/mongo";
+import { Meteor } from "meteor/meteor";
 
 const Items = new Mongo.Collection("items");
+
+populate = () => {
+  while (Items.find().count() < 10) {
+    Items.insert({
+      name: faker.commerce.product(),
+      image: faker.image.food(),
+      likes: 0,
+      dislikes: 0,
+      wantMore: 0,
+      wantLess: 0
+    });
+  }
+};
+
+Meteor.methods({
+  "Items.addOne": ({ name }) => {
+    return Items.insert({ name });
+  },
+  "Item.like": ({ id }) => {
+    return Items.update({ _id: id }, { $inc: { likes: 1 } });
+  },
+  "Item.dislike": ({ id }) => {
+    return Items.update({ _id: id }, { $inc: { dislikes: 1 } });
+  },
+  "Item.wantMore": ({ id }) => {
+    return Items.update({ _id: id }, { $inc: { wantMore: 1 } });
+  },
+  "Item.wantLess": ({ id }) => {
+    return Items.update({ _id: id }, { $inc: { wantLess: 1 } });
+  }
+});
+
+Meteor.publish("items", () => {
+  return Items.find();
+});
 
 export default Items;
