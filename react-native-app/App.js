74051e63aafb5b82ac3c66d816939ce9c8e4077c
Alexandre <alexandreguilngar@gmail.com>
2018-11-21 00:58:32 +0100

    implemented react native deck swiper

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -6,88 +6,88 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
+  Button
 } from "react-native";
 import { WebBrowser } from "expo";
 import Meteor, { withTracker } from "react-native-meteor";
+import Swiper from "react-native-deck-swiper";
 
 const SERVER_URL = "ws://localhost:3000/websocket";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      swipedAllCards: false,
+      swipeDirection: "",
+      isSwipingBack: false,
+      cardIndex: 0
+    };
+  }
   componentDidMount() {
     Meteor.connect(SERVER_URL);
   }
 
-  onPressButton = (id, buttonName) => {
-    console.log("id : ");
-    console.log(id);
-    console.log("buttonName : ");
-    console.log(buttonName);
+  onSwipe = (id, buttonName) => {
+    console.log("id : " + id);
+    console.log("buttonName : " + buttonName);
+
+    this.setState({
+      cardIndex: this.state.cardIndex + 1
+    });
 
     Meteor.call(`Item.${buttonName}`, { id }, (err, res) => {
       console.log(`Item.${buttonName}`, err, res);
     });
-    console.log(this.props.items);
   };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.helpContainer}>
-          <Text style={styles.instructions}>
-            Item Count: {this.props.count}
-          </Text>
-          <TouchableOpacity style={styles.button} onPress={this.handleAddItem}>
-            <Text>Add Item</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          {!!this.props.items ? this.renderCards() : <Text>No item</Text>}
-        </View>
-      </View>
-    );
+    console.log("RENDERING");
+    console.log(this.props.items);
+    return <View style={styles.container}>{this.renderSwiper()}</View>;
   }
 
-  renderCards() {
-    return this.props.items.map(item => {
-      return (
-        <View key={item._id}>
-          <Image
-            style={{ width: 200, height: 200 }}
-            source={{ uri: item.image }}
-          />
-          <Text>{item.name}</Text>
-          <TouchableOpacity
-            style={styles.button}
-            name="like"
-            onPress={() => this.onPressButton(item._id, "like")}
-          >
-            <Text>Like {item.likes}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            name="dislike"
-            onPress={() => this.onPressButton(item._id, "dislike")}
-          >
-            <Text>Dislike {item.dislikes}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            name="wantMore"
-            onPress={() => this.onPressButton(item._id, "wantMore")}
-          >
-            <Text>Want more {item.wantMore}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            name="wantLess"
-            onPress={() => this.onPressButton(item._id, "wantLess")}
-          >
-            <Text>Want less {item.wantLess}</Text>
-          </TouchableOpacity>
-        </View>
-      );
-    });
+  renderSwiper() {
+    console.log("this.state.cardIndex: " + this.state.cardIndex);
+    console.log(" ");
+    return (
+      <Swiper
+        cards={this.state.cards}
+        renderCard={card => {
+          return (
+            <View style={styles.card}>
+              <Image
+                style={{ flex: 1 }}
+                source={{
+                  uri:
+                    "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1379.jpg"
+                }}
+              />
+            </View>
+          );
+        }}
+        onSwipedLeft={cardIndex => {
+          this.onSwipe(cardIndex + 1, "dislike");
+        }}
+        onSwipedRight={cardIndex => {
+          this.onSwipe(cardIndex + 1, "like");
+        }}
+        onSwipedTop={cardIndex => {
+          this.onSwipe(cardIndex + 1, "wantMore");
+        }}
+        onSwipedBottom={cardIndex => {
+          this.onSwipe(cardIndex + 1, "wantLess");
+        }}
+        onSwipedAll={() => {
+          console.log("onSwipedAll");
+        }}
+        cardIndex={this.state.cardIndex}
+        backgroundColor={"#FFFFFF"}
+        stackSize={2}
+      />
+    );
   }
 }
 
@@ -102,8 +102,19 @@ export default withTracker(params => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#FFFFFF"
+  },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
   }
 });
