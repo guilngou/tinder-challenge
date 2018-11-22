e0130f0164ee46c18a4b5664cc0962138f0760e7
Alexandre <alexandreguilngar@gmail.com>
2018-11-22 11:26:41 +0100

    stable version with mocked items

--- meteor/imports/api/items.js
+++ meteor/imports/api/items.js
@@ -12,8 +12,9 @@ Meteor.methods({
     return Items.update({ _id: `${id}` }, { $inc: { likes: 1 } });
   },
   "Item.dislike": ({ id }) => {
-    console.log(Items.findOne({ _id: `${id}` }));
-    return Items.update({ _id: `${id}` }, { $inc: { dislikes: 1 } });
+    console.log(id);
+    console.log(Items.findOne({ _id: id }));
+    return Items.update({ _id: id }, { $inc: { dislikes: 1 } });
   },
   "Item.wantMore": ({ id }) => {
     console.log(Items.findOne({ _id: `${id}` }));
@@ -30,19 +31,96 @@ Meteor.publish("items", () => {
 });
 
 populate = () => {
-  let i = 0;
-  while (Items.find().count() < 10) {
-    Items.insert({
-      _id: `${++i}`,
-      name: "Chips de Banane",
-      image:
-        "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1379.jpg",
-      likes: 0,
-      dislikes: 0,
-      wantMore: 0,
-      wantLess: 0
-    });
-  }
+  Items.insert({
+    name: "Chips de Banane",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1379.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Mix Fruits secs",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1837.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Amandes",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1843.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Chocolate Kiss",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1366-min.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Orange",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A2265-min.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Smoothie",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A2086-min.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Thé Vert Jasmin",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A4721-min.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Cookie Artisanal",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1523-min.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Pomme",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A2253-min.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
+  Items.insert({
+    name: "Café Moka Lekempti",
+    image:
+      "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A2204-min.jpg",
+    likes: 0,
+    dislikes: 0,
+    wantMore: 0,
+    wantLess: 0
+  });
 };
 
 export default Items;
--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,15 +1,5 @@
 import React from "react";
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Button
-} from "react-native";
-import { WebBrowser } from "expo";
+import { Image, StyleSheet, Text, View } from "react-native";
 import Meteor, { withTracker } from "react-native-meteor";
 import Swiper from "react-native-deck-swiper";
 
@@ -19,7 +9,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      // cards: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      cards: [{ name: "test1", id: 1 }, { name: "test2", id: 2 }],
       swipedAllCards: false,
       swipeDirection: "",
       isSwipingBack: false,
@@ -30,23 +21,30 @@ class App extends React.Component {
     Meteor.connect(SERVER_URL);
   }
 
-  onSwipe = (id, buttonName) => {
-    console.log("id : " + id);
+  onSwipe = (cardIndex, buttonName) => {
+    console.log("cardIndex : " + cardIndex);
     console.log("buttonName : " + buttonName);
 
-    this.setState({
-      cardIndex: this.state.cardIndex + 1
-    });
+    console.log(this.props.items[cardIndex]);
+    const id = this.props.items[cardIndex]._id;
 
     Meteor.call(`Item.${buttonName}`, { id }, (err, res) => {
       console.log(`Item.${buttonName}`, err, res);
+
+      if (res) {
+        this.setState({
+          cardIndex: this.state.cardIndex + 1
+        });
+      }
     });
   };
 
   render() {
-    console.log("RENDERING");
-    console.log(this.props.items);
-    return <View style={styles.container}>{this.renderSwiper()}</View>;
+    return (
+      <View style={styles.container}>
+        {!!this.props.items ? this.renderSwiper() : <Text>No item</Text>}
+      </View>
+    );
   }
 
   renderSwiper() {
@@ -54,31 +52,154 @@ class App extends React.Component {
     console.log(" ");
     return (
       <Swiper
-        cards={this.state.cards}
+        // cards={this.state.cards}
+        cards={this.props.items}
+        overlayLabels={{
+          bottom: {
+            element: (
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "red",
+                  color: "red",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                WANT LESS
+              </Text>
+            ),
+            title: "WANT LESS",
+            style: {
+              label: {
+                backgroundColor: "black",
+                borderColor: "black",
+                color: "white",
+                borderWidth: 1
+              },
+              wrapper: {
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
+              }
+            }
+          },
+          left: {
+            element: (
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "red",
+                  color: "red",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                NOPE
+              </Text>
+            ),
+            title: "NOPE",
+            style: {
+              label: {
+                backgroundColor: "black",
+                borderColor: "black",
+                color: "white",
+                borderWidth: 1
+              },
+              wrapper: {
+                flexDirection: "column",
+                alignItems: "flex-end",
+                justifyContent: "flex-start",
+                marginTop: 30,
+                marginLeft: -30
+              }
+            }
+          },
+          right: {
+            element: (
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "green",
+                  color: "green",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                LIKE
+              </Text>
+            ),
+            title: "LIKE",
+            style: {
+              wrapper: {
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "flex-start",
+                marginTop: 30,
+                marginLeft: 30
+              }
+            }
+          },
+          top: {
+            element: (
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "green",
+                  color: "green",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                WANT MORE
+              </Text>
+            ),
+            title: "WANT MORE",
+            style: {
+              label: {
+                backgroundColor: "black",
+                borderColor: "black",
+                color: "white",
+                borderWidth: 1
+              },
+              wrapper: {
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
+              }
+            }
+          }
+        }}
         renderCard={card => {
           return (
             <View style={styles.card}>
               <Image
                 style={{ flex: 1 }}
                 source={{
-                  uri:
-                    "https://s3.eu-central-1.amazonaws.com/totems3/products/3C5A1379.jpg"
+                  uri: card.image
                 }}
               />
+              <Text>
+                {card.name} ID : {card._id}
+              </Text>
             </View>
           );
         }}
         onSwipedLeft={cardIndex => {
-          this.onSwipe(cardIndex + 1, "dislike");
+          this.onSwipe(cardIndex, "dislike");
         }}
         onSwipedRight={cardIndex => {
-          this.onSwipe(cardIndex + 1, "like");
+          this.onSwipe(cardIndex, "like");
         }}
         onSwipedTop={cardIndex => {
-          this.onSwipe(cardIndex + 1, "wantMore");
+          this.onSwipe(cardIndex, "wantMore");
         }}
         onSwipedBottom={cardIndex => {
-          this.onSwipe(cardIndex + 1, "wantLess");
+          this.onSwipe(cardIndex, "wantLess");
         }}
         onSwipedAll={() => {
           console.log("onSwipedAll");
@@ -106,6 +227,7 @@ const styles = StyleSheet.create({
   },
   card: {
     flex: 1,
+    elevation: 5,
     borderRadius: 4,
     borderWidth: 2,
     borderColor: "#E8E8E8",
